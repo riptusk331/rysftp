6609bc4f95b407cf8dd9fbcea4d80c317de0d2ed
Ryan Joyce <ryan.joyce.88@gmail.com>
2021-01-13 13:49:34 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
     setuptools.setup(
         name="RySftp",
-        version="0.0.4",
+        version="0.0.5",
         author="Ryan P Joyce",
         author_email="ryan.joyce.88@gmail.com",
         description="simple, asynchronous sftp operations",
