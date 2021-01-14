a604bce5bf732520a2408901e13f0f2adc262dc4
Ryan Joyce <ryan.joyce.88@gmail.com>
2021-01-14 14:50:14 -0500

    add uploading to tests

--- tests/test.py
+++ tests/test.py
@@ -22,15 +22,20 @@ ry = RySftp()
 how_much = int(sys.argv[1])
 remotedir = getenv("RYSFTP_REMOTEDIR")
 with ry(remotedir):
-    t1 = time.time()
-    whatwegot = ry.download_latest(how_much)
-    t2 = time.time()
+    # t1 = time.time()
+    # whatwegot = ry.download_latest(how_much)
+    # t2 = time.time()
 
-    remote = sorted(ry._sftp.listdir_attr(), key=lambda x: x.st_mtime, reverse=True)
-    to_download = [f.filename for f in remote if S_ISREG(f.st_mode)][:how_much]
-    t3 = time.time()
-    for dl in to_download:
-        ry._sftp.get(dl, f"./synchronous-{dl}")
-    t4 = time.time()
-    logging.debug(f"Asynchronous Download: {len(whatwegot)} files in {t2-t1} seconds")
-    logging.debug(f"Synchronous Download: {len(to_download)} files in {t4-t3} seconds")
+    # remote = sorted(ry._sftp.listdir_attr(), key=lambda x: x.st_mtime, reverse=True)
+    # to_download = [f.filename for f in remote if S_ISREG(f.st_mode)][:how_much]
+    # t3 = time.time()
+    # for dl in to_download:
+    #     ry._sftp.get(dl, f"./synchronous-{dl}")
+    # t4 = time.time()
+    # logging.debug(f"Asynchronous Download: {len(whatwegot)} files in {t2-t1} seconds")
+    # logging.debug(f"Synchronous Download: {len(to_download)} files in {t4-t3} seconds")
+    
+    t5 = time.time()
+    uploaded = ry.upload_latest(how_much, ['.dat'])
+    t6 = time.time()
+    logging.debug(f"Asynchronous Upload: {len(uploaded)} files in {t6-t5} seconds")
